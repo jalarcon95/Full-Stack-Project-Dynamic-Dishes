bf0a9531fcd5e3fa636abcf764690dca1fa33441
Cwill290 <cwill290@gmail.com>
2023-07-26 20:57:47 -0400

    database basics still more to add. got connection working

---
+++ index.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+const sequelize = require('./config/db');
+const Recipe = require('./models/recipe');
+dotenv.config();
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+
+    // Your further code and logic can go here
+
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
