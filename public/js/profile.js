a8b5d906a343d5814e6ebbc85f6f8048e8fdbbc2
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-07-29 20:19:54 -0400

    debugging

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,15 +1,15 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#recipe-title').value.trim();
+    const title = document.querySelector('#recipe-title').value.trim();
+    const description = document.querySelector('#recipe.descriptioin').value.trim();
     const ingredients = document.querySelector('#recipe-ingredients').value.trim();
     const instructions = document.querySelector('#recipe-instructions').value.trim();
 
-
-    if (title && description) {
+    if (title && description && ingredients && instructions) {
       const response = await fetch(`/api/recipes`, {
         method: 'POST',
-        body: JSON.stringify({ name, ingredients, instructions }),
+        body: JSON.stringify({ title, description, ingredients, instructions }),
         headers: {
           'Content-Type': 'application/json',
         },
