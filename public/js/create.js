e7c322ce9aafd6c3d07ef86413098de6941d6005
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-07-31 09:11:17 -0400

    added create.js file

---
+++ public/js/create.js
@@ -0,0 +1,46 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#recipe-title').value.trim();
+    const description = document.querySelector('#recipe-description').value.trim();
+    const ingredients = document.querySelector('#recipe-ingredients').value.trim();
+    const instructions = document.querySelector('#recipe-instructions').value.trim();
+
+    if (title && description && ingredients && instructions) {
+      const response = await fetch(`/api/blog`, {
+        method: 'POST',
+        body: JSON.stringify({ title, description, ingredients, instructions }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to create blog');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blog/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete blog');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-recipe-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document.querySelector('.recipe-list')?.addEventListener('click', delButtonHandler);
