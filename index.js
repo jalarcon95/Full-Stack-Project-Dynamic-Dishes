f7be30a5f0935f2ce2c5ae02cd74c9a4a1269e4b
Karen <kavictoria2910@hotmail.com>
2023-07-31 20:55:49 -0400

    Login Logout API

--- index.js
+++ index.js
@@ -3,43 +3,15 @@ const sequelize = require('./config/connection');
 const Recipe = require('./models/recipe');
 dotenv.config();
 
-const express = require('express')
-const app = express()
-const port = 3000
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+ //Your further code and logic can go here
 
-app.get('/login', async (req, res) => {
-  // const  recipe =  await Recipe.create ({
-  //   title:'test',
-  //   description:'text',
-  //   ingredients:'a',
-  //   instructions:'instructions'
-  // });
-  // recipe.save();
-  // res.send('saved')
-  res.render('views/login.handlebars')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-sequelize.sync();
-
-
-
-// (async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Connection has been established successfully.');
-
-//     // Your further code and logic can go here
-
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-// })();
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+   }
+ })();
 
