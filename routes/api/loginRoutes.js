8d89a3ec0ccf448fb3e2c0c6011652af7cc7e8a9
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-08-01 18:26:38 -0400

    debugging api connection to front end signup and login

--- routes/api/loginRoutes.js
+++ routes/api/loginRoutes.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
+// const express = require('express');
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const cors = require('cors');
 
-const app = express();
-const port = 3000;
-const secretKey = 'your-secret-key';
-const users = [];
+// const app = express();
+// const port = 3000;
+// const secretKey = 'your-secret-key';
+// const users = [];
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
-app.post('/api/register', async (req, res) => {
-  const { username, password } = req.body;
-  if (users.some(user => user.username === username))
-    return res.status(409).json({ message: 'Username already exists.' });
+// app.post('/api/register', async (req, res) => {
+//   const { username, password } = req.body;
+//   if (users.some(user => user.username === username))
+//     return res.status(409).json({ message: 'Username already exists.' });
 
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    users.push({ username, password: hashedPassword });
-    res.status(201).json({ message: 'Registration successful.' });
-  } catch (error) {
-    res.status(500).json({ message: 'Registration failed.' });
-  }
-});
+//   try {
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     users.push({ username, password: hashedPassword });
+//     res.status(201).json({ message: 'Registration successful.' });
+//   } catch (error) {
+//     res.status(500).json({ message: 'Registration failed.' });
+//   }
+// });
 
-app.post('/api/login', async (req, res) => {
-  const { username, password } = req.body;
-  const user = users.find(user => user.username === username);
-  if (!user || !(await bcrypt.compare(password, user.password)))
-    return res.status(401).json({ message: 'Invalid credentials.' });
+// app.post('/api/login', async (req, res) => {
+//   const { username, password } = req.body;
+//   const user = users.find(user => user.username === username);
+//   if (!user || !(await bcrypt.compare(password, user.password)))
+//     return res.status(401).json({ message: 'Invalid credentials.' });
 
-  const token = jwt.sign({ username: user.username }, secretKey, { expiresIn: '1h' });
-  res.json({ message: 'Login successful.', token });
-});
+//   const token = jwt.sign({ username: user.username }, secretKey, { expiresIn: '1h' });
+//   res.json({ message: 'Login successful.', token });
+// });
 
-app.post('/api/logout', (req, res) => {
-  return res.json({ message: 'Logout successful.' });
-});
+// app.post('/api/logout', (req, res) => {
+//   return res.json({ message: 'Logout successful.' });
+// });
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+// app.listen(port, () => console.log(`Server listening on port ${port}`));
